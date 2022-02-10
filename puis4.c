01477a301c946e83d099df6b57baa70c60f744f7
momoladebrouill <momoladebrouill@gmail.com>
2022-02-10 10:29:22 +0100

    ça fonctionne yaay

--- puis4.c
+++ puis4.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-const char vide[2]="_";
 char tabl[64];
+int tour=0;
+const char val[4]= "XO_";
 
-void setat(int x, int y, char val){
-  tabl[x+y*8]=val;
+void setat(int x, int y, int tour){
+  tabl[x+y*8]=val[tour];
 }
 
 char getat(int x,int y){
@@ -13,41 +14,41 @@ char getat(int x,int y){
 }
 
 int visualize(){
-  printf("\n");
+  printf("\t  0 1 2 3 4 5 6 7\n");
   for (int i = 0; i < 8; i++) {
-    printf("\t");
+    printf("\t |");
     for (int j = 0; j < 8; j++) {
       printf("%c ",getat(i,j));
     }
-    printf("\n");
+    printf("|\n");
   }
   printf("\n");
   return 0;
 };
 
-int putatcolumn(int col,char *val){
+int putatcolumn(int col,int tour){
   int where=7;
-  while(getat(where,col)!=*vide && where>=0){
+  while(getat(where,col)!=val[3] && where>=0){
     where--;
   }
-  setat(where,col,*val);
+  setat(where,col,tour);
 }
 int main() {
   printf("\n=== Execution del code ===\n");
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      setat(i,j,*vide);
+      setat(i,j,3);
     }
   }
-  int a=0;
-  char val[2]= "t";
-  while (a>=0) {
+  int lieu=0;
+
+
+  while (lieu>=0) {
     visualize();
-    printf("Un char stp boto : ");
-    scanf("%2s", val);
     printf("Un lieu stp bata : ");
-    scanf("%2d",&a);
-    putatcolumn(a,val);
+    scanf("%2d",&lieu);
+    tour=(tour+1)%2;
+    putatcolumn(lieu,tour);
 
 
   }
