4bab0150d40a2ef838b09cf0768b1e144a2a668e
momoladebrouill <momoladebrouill@gmail.com>
2022-02-14 11:24:10 +0100

    essai sur ordi blanc

--- puis4.c
+++ puis4.c
@@ -7,11 +7,11 @@ const char val[4]= "XO_";
 
 void setat(int x, int y, int tour){
   tabl[x+y*8]=val[tour];
-}
+};
 
 char getat(int x,int y){
   return tabl[x+y*8];
-}
+};
 
 int visualize(){
   printf("\t  0 1 2 3 4 5 6 7\n");
@@ -28,29 +28,27 @@ int visualize(){
 
 int putatcolumn(int col,int tour){
   int where=7;
-  while(getat(where,col)!=val[3] && where>=0){
+  while(getat(where,col)!=val[2] && where>=0){
     where--;
   }
   setat(where,col,tour);
-}
+};
+
 int main() {
   printf("\n=== Execution del code ===\n");
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      setat(i,j,3);
+      setat(i,j,2);
     }
   }
   int lieu=0;
-
-
   while (lieu>=0) {
     visualize();
     printf("Un lieu stp bata : ");
     scanf("%2d",&lieu);
     tour=(tour+1)%2;
     putatcolumn(lieu,tour);
-
-
   }
   return 0;
-}
+};
+
