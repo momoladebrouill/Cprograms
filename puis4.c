45c4ff7cf07f620c86351ae092de2c8b057f788d
momoladebrouill <momoladebrouill@gmail.com>
2022-02-14 11:22:26 +0100

    ajout de check si c'est puis4

--- puis4.c
+++ puis4.c
@@ -3,14 +3,40 @@
 
 char tabl[64];
 int tour=0;
-const char val[4]= "XO_";
+const char val[4]= "XO ";
 
 void setat(int x, int y, int tour){
   tabl[x+y*8]=val[tour];
 }
 
 char getat(int x,int y){
-  return tabl[x+y*8];
+  if(x>=0 && x<8 && y>=0 && y<8){
+    return tabl[x+y*8];
+  }
+  return val[3];
+
+}
+int checkforpui(int x, int y){
+  int i,som,xx,yy;
+  for (int dx = -1; dx < 2; dx++) {
+    for (int dy = -1; dy < 2; dy++) {
+      i=1;
+      while(getat(x,y)==getat(x+dx*i,y+dy*i) && i<5){
+        i++;
+      }
+      som=0;
+      xx=x+dx*i;
+      yy=x+dy*i;
+      for(int j=0; j<4;j++){
+        som+=getat(xx,yy)==getat(xx-dx*j,yy-dy*j);
+      }
+      printf("Depuis (%d,%d) il y'a le même char %dfois\n",xx,yy,som );
+      if (som==4) {
+        return 1;
+      }
+    }
+    return 0;
+  }
 }
 
 int visualize(){
@@ -28,16 +54,18 @@ int visualize(){
 
 int putatcolumn(int col,int tour){
   int where=7;
-  while(getat(where,col)!=val[3] && where>=0){
+  while(getat(where,col)!=val[2] && where>=0){
     where--;
   }
   setat(where,col,tour);
+  printf("Il y'a un puissance 4: %d\n",checkforpui(where,col));
 }
+
 int main() {
   printf("\n=== Execution del code ===\n");
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      setat(i,j,3);
+      setat(i,j,2);
     }
   }
   int lieu=0;
@@ -45,9 +73,9 @@ int main() {
 
   while (lieu>=0) {
     visualize();
-    printf("Un lieu stp bata : ");
-    scanf("%2d",&lieu);
     tour=(tour+1)%2;
+    printf("Un lieu stp bata (%c) : ",val[tour]);
+    scanf("%2d",&lieu);
     putatcolumn(lieu,tour);
 
 
