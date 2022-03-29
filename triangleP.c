02c86fe7220a439e6f75740fff2afa0f8e1c02d8
momoladebrouill <momoladebrouill@gmail.com>
2022-03-29 15:53:24 +0200

    Update triangleP.c

--- triangleP.c
+++ triangleP.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculateSum(int num[],int size);
-void printls(int num[],int size);
-int trianglePascal(int nb);
-
-int main() {
-  trianglePascal(10);
-  return 0;
-}
-
 void printls(int num[], int size){
   for (size_t i = 0; i < size; i++) {
     printf("%d\t",num[i] );
@@ -18,19 +9,25 @@ void printls(int num[], int size){
 }
 
 int trianglePascal(int nb){
-  int s[]={1};
-  for (int i = 1; i < nb; i++) {
-    int next[i+1];
-    next[0]=1;
-    next[i]=1;
-
+  int a[100];
+  int b[100];
+  for (int i = 0; i < nb; i++) {
+    a[0]=1;
+    a[i]=1;
+    b[0]=1;
+    b[i]=1;
     for (int j = 1; j < i-1 ; j++) {
-      next[j]=s[j-1]+s[j];
+      b[j]=a[j-1]+a[j];
     }
-    int s[i+1];
+
     for(int j = 0; j < i +1; j++){
-      s[j]=next[j];
+      a[j]=b[j];
     }
-    printls(s,i);
+    printls(a,i);
   }
+}
+
+int main() {
+  trianglePascal(10);
+  return 0;
 }
