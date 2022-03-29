a0aa981840623e2dee2e7946590511bbafb4489a
momoladebrouill <momoladebrouill@gmail.com>
2022-03-29 15:43:05 +0200

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define taille 10
+
+int * gen(){
+   static int r[taille];
+   for ( int i = 0; i < taille; ++i) {
+      r[i] = rand()%50;
+   }
+   return r;
+}
+
+void bubblesort(int *l,size_t len){
+    int ismexe=1;
+    while(ismexe){
+        ismexe=0;
+        for(size_t i=0;i<len-1;i++){
+            if(l[i]>l[i+1]){
+                ismexe=1;
+                l[i]=l[i]^l[i+1];
+                l[i+1]=l[i]^l[i+1];
+                l[i]=l[i]^l[i+1];
+            }
+        }
+    }
+} 
+
+void printls(int *l,size_t len){
+    for ( int i = 0; i < len; i++ ) {
+      printf( "%d %i\n", i,l[i]);
+   }
+}
+
+int main (){
+   int *a;
+   a=gen();
+   printf("avant\n");
+   printls(a,10);
+   bubblesort(a,10);
+   printf("apres\n");
+   printls(a,10);
+   return 0;
+}
