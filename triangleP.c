6f671b6d639c12a31c53089b97e2154cb09d3a89
momoladebrouill <momoladebrouill@gmail.com>
2022-03-30 18:28:46 +0200

    prettier :sparkles:

--- triangleP.c
+++ triangleP.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define nbetages 25
 void printls(int num[], int size){
   for (size_t i = 0; i < size; i++) {
     printf("%d\t",num[i] );
@@ -9,25 +9,22 @@ void printls(int num[], int size){
 }
 
 int trianglePascal(int nb){
-  int a[100];
-  int b[100];
-  for (int i = 0; i < nb; i++) {
-    a[0]=1;
-    a[i]=1;
-    b[0]=1;
-    b[i]=1;
+  static int a[nbetages]={1};
+  static int b[nbetages];
+  for (int i = 1; i < nb; i++) {
     for (int j = 1; j < i-1 ; j++) {
       b[j]=a[j-1]+a[j];
     }
-
-    for(int j = 0; j < i +1; j++){
-      a[j]=b[j];
-    }
+    b[0]=1;
+    b[i-1]=1;
+    for(int j = 0; j < i; j++) a[j]=b[j]; // le C c'est sorcier des fois
+    printf("n%i :",i);
     printls(a,i);
   }
 }
 
 int main() {
-  trianglePascal(10);
+  printf("Triangle de Pascal pour %i étages:\n",nbetages );
+  trianglePascal(nbetages);
   return 0;
 }
