7e1e8ac77aae59099e832f3852f3da465d89dbbd
momoladebrouill <momoladebrouill@gmail.com>
2022-04-06 09:41:55 +0200

    Update binarytree.c

--- niveau 2/binarytree.c
+++ niveau 2/binarytree.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 #define size malloc(sizeof(node))
 
 typedef struct node {
@@ -9,22 +8,28 @@ typedef struct node {
     struct node * d;
 } node;
 
-
-
-node * generate(node * r, int val){
-    r->v=val;
-    int vg=val-2,vd=val-1;
-
-    if (vg>0){
-      r->g=(node*) size;
-      generate(r->g,vg);
-    }else{r->g=0;} //maybe use less
-
-    if (vd>0){
-      r->d=(node*) size;
-      generate(r->d,vd);
-    }else{r->d=0;} // so this does
-
+static int l1,l2;
+node * generate( node * r, int * list, int len){
+    l1=(int) len/2+len%2;
+    l2=(int) len/2;
+    
+    r->v = &(list);
+    if(l1>0){
+        r->g = (node * ) size;
+        generate(r->g,list,l1-1);
+    }
+    
+    if(l2>0){
+        r->d = (node*) size;
+        generate(r->d,*(list+l1),l2-1);
+    }
+    
+    /*if(l2){
+        r->d = (node*) size;
+        int *deb=(val+l1);
+        generate(r->d,deb,l2);
+    }*/
+    
     return r;
 }
 
@@ -41,18 +46,26 @@ int check(node *r){
 void print(node *r,int tabs){
     for(int i=0;i<tabs;i++)printf("---|");
     printf("%i\n",r->v);
-    if(r->d!=0 && r->g!=0){
+    if(r->d!=0){
         print(r->d,tabs+1);
+    }
+    if(r->g!=0){
         print(r->g,tabs+1);
     }
 }
 
 int main() {
-    srand(time(NULL));
-
     node * b = (node *) size;
-    generate(b,5);
-    printf("%i\n",check(b));
+    /*b->g=(node*)size;
+    b->d=(node*)size;
+    b->g->v=1;
+    b->d->v=3;
+    b->v=2;
+    b->g->g=(node*)size;
+    b->g->g->v=5;*/
+    static int elems={1,2,3,4,5,6,7,8};
+    generate(b,elems,5);
+    printf("Iel est bien : %i\n",check(b));
     print(b,0);
     return 0;
 }
