1646b19268b15c9d703ca7a7e6399bdc6f377866
momoladebrouill <momoladebrouill@gmail.com>
2022-04-09 16:22:36 +0200

    ajout de la fonction add mais c'est bizarre ça marche tout le temps ???

--- niveau 2/binarytree.c
+++ niveau 2/binarytree.c
@@ -14,46 +14,72 @@ void generate( node * r, int * list,int deb, int fin){
     r->v = list[deb+mid];
     r->g=(node*)size;
     r->d=(node*)size;
-    if(len==3){
-      r->g->v=list[deb+0];
-      r->d->v=list[deb+2];
-    }
-    else if(len>3){
+
+    if(len>3){
       generate(r->g,list,deb,mid-1);
       generate(r->d,list,mid+1,fin);
+    }else if(len==3){
+      r->g->v=list[deb+0];
+      r->d->v=list[deb+2];
+    }else if(len==2){
+      r->g->v=list[deb];
     }else{
-      printf("Liste trop courte rip\n");
+      printf("Liste trop courte : %i\n",len);
     }
-
+}
+void add(node *r, int v){
+  if (r->v > v) {
+    if(r->d){
+      add(r->d,v);
+    }else{
+      r->d=(node*) size;
+      r->d->v=v;
+    }
+  }else{
+    if(r->g){
+      add(r->g,v);
+    }else{
+      r->g=(node*) size;
+      r->g->v=v;
+    }
+  }
 }
 
 int check(node *r){
-    if(r->g == 0 || r->d == 0){
-        return 1;
+  int g=1,d=1;
+    if(r->g){
+      if(r->g->v < r->v){
+        g=check(r->g);
+      }
     }
-    if((r->g->v < r->v) && (r->v < r->d->v) ){
-      return check(r->g) && check(r->d);
+    if(r->d){
+      if(r->d->v > r->v){
+        g=check(r->d);
+      }
     }
-    return 0;
+    return g&&d;
 }
 
-void print(node *r,int tabs){
-    for(int i=0;i<tabs;i++)printf("---|");
+void print(node *r,int tabs,char side){
+    for(int i=0;i<tabs-1;i++)printf("---|");
+    if(tabs)printf("--%c|", side);
     printf("%i\n",r->v);
     if(r->d!=0){
-        print(r->d,tabs+1);
+        print(r->d,tabs+1,'d');
     }
     if(r->g!=0){
-        print(r->g,tabs+1);
+        print(r->g,tabs+1,'g');
     }
 }
 
 int main() {
     node * b = (node *) size;
-
-    static int elems[]={1,2,3,4,5,6,7};
+    static int elems[]={1*2,2*2,3*2,4*2,5*2,6*2,7*2};
     generate(b,elems,0,6);
-    //printf("Iel est bien : %i\n",check(b));
-    print(b,0);
+    printf("Iel est bien : %i\n",check(b));
+    print(b,0,0);
+    add(b,3);
+    print(b,0,0);
+    printf("Iel est bien : %i\n",check(b));
     return 0;
 }
