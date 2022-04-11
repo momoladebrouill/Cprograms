548b3a86cf58e322f081a7e9d5c3cade5575ef39
rayou <rayou@vieuxblanc.home>
2022-04-11 09:33:17 +0200

    cleaner

--- niveau 2/mergesort.c
+++ niveau 2/mergesort.c
@@ -4,8 +4,8 @@
 #define taille 10
 
 int printls(int *l,size_t len){
-    for ( int i = 0; i < len; i++ ) {
-      printf( "%i ",l[i]);
+    for(int i = 0; i < len; i++){
+      printf("%i ", l[i]);
    }
    printf("\n");
    return 0;
