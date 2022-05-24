1c6074d96b1b2991cd1ebb094f6b4795549c3502
rayou <momoladebrouill@gmail.com>
2022-05-24 19:36:19 +0200

    clean even avec le check mmry

--- niveau 2/prime.c
+++ niveau 2/prime.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#define WIDTH 20
+#define WIDTH 24
 #define HEIGHT 10
 //2,3,5,7,11,13,17,19,23,29,37 ...
 const int primes[]={1,2,3,5,7,11,13,17,19,23,29,37};
 void create(int list[WIDTH][HEIGHT]){
+	int nbofprimes=1;
 	for(int i=1;i<WIDTH;i++){
 		list[i][0]=i;
-		for(int j=1;j<HEIGHT;j++){
-			int nb=0;
-			int div=primes[j];
-			while((i/div)*div==i){// tant que i est divisible par nb
-				nb++;// c'est qu'il est encore divible
-				div=div*primes[j];//donc on augmente la puissance
+		int nb=i;
+		while(nb>1){
+			int NoDivs=1;
+			for(int j=i-1;j>1;j--){//pour chaque nombre précédent
+				if((nb/j)*j==nb){// si nb est divisble par j
+					nb/=j; //on le divise par j
+					NoDivs=0;
+					for(int k=1;k<HEIGHT;++k){list[i][k]+=list[j][k];}
+						// On ajoute les diviseurs de celui trouvé
+				}
 			}
-			list[i][j]=nb;
-		}	
+			if(NoDivs){
+				list[i][nbofprimes]++;
+				nb/=nb;
+				nbofprimes++;
+			}
+
+		}
 	}
 }
 	
@@ -26,7 +36,7 @@ int main(){
 		create(list);
 		for(int i=0;i<WIDTH*HEIGHT;i++){
 			if(i%WIDTH==0){
-				printf("\n\033[1;33m%i\033[0m\t",primes[i/WIDTH]);
+				printf("\n\033[1;33m%2i\033[0m ",primes[i/WIDTH]);
 			}else{
 				printf("%2i ",list[i%WIDTH][i/WIDTH]);
 			}
